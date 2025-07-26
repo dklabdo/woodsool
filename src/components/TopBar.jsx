cc2bd1c8f1a3307d1c71cc75392d8fac66c68525
dklabdo <sayah.abdo22@gmail.com>
2025-07-26 23:35:16 +0100

    first test

--- src/components/TopBar.jsx
+++ src/components/TopBar.jsx
@@ -6,7 +6,7 @@ const TopBar = () => {
     <div className="bg-main w-full text-white text-base flex flex-row items-center px-4 md:px-12 py-4 gap-4 md:gap-8 min-h-[50px]">
       <div className="flex items-center gap-2">
         <LucidePhone className="w-4 h-4 " />
-        <span className="tracking-wide text-xs md:text-sm">+213 6 99 65 44 23</span>
+        <span className="tracking-wide text-xs md:text-sm">+213 7 71 64 47 01</span>
       </div>
       <div className="flex items-center gap-2">
         <Instagram className="w-4 h-4 " />
