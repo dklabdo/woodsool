9520d1f2940ace586bedb272d0987fb9b7c18e7e
dklabdo <sayah.abdo22@gmail.com>
2025-07-26 23:56:08 +0100

    with insta link

--- src/components/TopBar.jsx
+++ src/components/TopBar.jsx
@@ -1,21 +1,28 @@
-import { Phone as LucidePhone } from 'lucide-react';
-import { Instagram } from 'lucide-react';
+import { Phone as LucidePhone } from "lucide-react";
+import { Instagram } from "lucide-react";
 
 const TopBar = () => {
   return (
     <div className="bg-main w-full text-white text-base flex flex-row items-center px-4 md:px-12 py-4 gap-4 md:gap-8 min-h-[50px]">
       <div className="flex items-center gap-2">
         <LucidePhone className="w-4 h-4 " />
-        <span className="tracking-wide text-xs md:text-sm">+213 7 71 64 47 01</span>
+        <span className="tracking-wide text-xs md:text-sm">
+          +213 7 71 64 47 01
+        </span>
       </div>
       <div className="flex items-center gap-2">
         <Instagram className="w-4 h-4 " />
-        <a href="https://instagram.com" target="_blank" rel="noopener noreferrer" className="tracking-wide text-xs md:text-sm hover:underline">
-          Woodsool
+        <a
+          href="https://www.instagram.com/woodsoul_dz?igsh=NDR2bWJzZ2sybGQ="
+          target="_blank"
+          rel="noopener noreferrer"
+          className="tracking-wide text-xs md:text-sm hover:underline"
+        >
+          WoodSoul Dz
         </a>
       </div>
     </div>
   );
 };
 
-export default TopBar; 
+export default TopBar;
